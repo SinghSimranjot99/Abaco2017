6af782dc3da591dd0e832533a783b8b3b5ef3167
SinghSimranjot99 <simranjotsingh89@gmail.com>
2017-06-06 14:58:55 +0200

    StringaPalindroma_EDIT_conBanchmark

--- StringaPalindromaMain.cpp
+++ StringaPalindromaMain.cpp
@@ -1,10 +1,10 @@
-#include <iostream>s
-#include <stdio.h>
+#include <iostream>
+#include <algorithm>
 #include <string>
 #include <vector>
-#include <time.h>
+#include <ctime>
 
-#define BENCH_ITERATIONS 100
+#define BENCH_ITERATIONS 1000000
 
 using namespace std;
 
@@ -14,65 +14,24 @@ bool checkPalindrome_v2(const string &theString);
 
 bool checkPalindrome_v3(const string &theString);
 
+int doTest(vector<string> &toCheck,bool (*checkPalindromeFunz)(const string&));
+
 int main(int argc, char **argv)
 {
-    /*string str;                         //CREAZIONE E INIZIALIZZAZIONE STRINGA
-	cout<<"Inserisci una stringa:"<<endl;
-	cin>>str;
-	while(cin.get()!='\n');
-	*/
-	cin.get();
-	clock_t t1_start,t1_stop,t2_start,t2_stop,t3_start,t3_stop;
-	double runTimeMillisec_1,runTimeMillisec_2,runTimeMillisec_3;
-	
 	vector<string> v1;
 	v1.push_back("ciao");
 	v1.push_back("anna");
 	v1.push_back("inipotinonavevanotopi");
 	v1.push_back("itopinonavevanonipoti");
+    
+	int runTimeMillisec_1 = doTest(v1,checkPalindrome_v1);
+	int runTimeMillisec_2 = doTest(v1,checkPalindrome_v2);
+	int runTimeMillisec_3 = doTest(v1,checkPalindrome_v3);
 	
-	
-	//cout<<endl<<"VERSIONE 1 :"<<endl<<endl;
-	t1_start = clock();
-	for(int i=0;i<BENCH_ITERATIONS;i++){
-		for(int j=0;j<v1.size();j++){
-		   checkPalindrome_v1(v1[j]); //PROVA VERSIONE 1 
-		}
-	}
-	t1_stop = clock();
-	
-	//cout<<endl<<"VERSIONE 2"<<endl<<endl;
-	t2_start = clock();
-	for(int i=0;i<BENCH_ITERATIONS;i++){
-	   for(int j=0;j<v1.size();j++){
-		   checkPalindrome_v2(v1[j]);  //PROVA VERSIONE 2 			
-		}
- 	}   
-	t2_stop = clock();
- 
-	//cout<<endl<<"VERSIONE 3"<<endl<<endl;
-	t3_start = clock();
-	for(int i=0;i<BENCH_ITERATIONS;i++){
-		for(int j=0;j<v1.size();j++){
-		   checkPalindrome_v3(v1[j]);  //PROVA VERSIONE 3 
-		}
-    }	
-	t3_stop = clock();
-	
-	runTimeMillisec_1 = (double)(t1_stop-t1_start)/CLOCKS_PER_SEC;
-	runTimeMillisec_2 = (double)(t2_stop-t2_start)/CLOCKS_PER_SEC;
-	runTimeMillisec_3 = (double)(t3_stop-t3_start)/CLOCKS_PER_SEC;
-	
-	cout<<"VERSIONE 1 TEMPO:"<<runTimeMillisec_1*1000<<endl;
-	cout<<"VERSIONE 2 TEMPO:"<<runTimeMillisec_2*1000<<endl;
-	cout<<"VERSIONE 3 TEMPO:"<<runTimeMillisec_3*1000<<endl;
-	
-	
-	/*if(equal(str.begin(),str.end(),str.rbegin()))  //confronta la sequenza di caratteri che si sono tra begin e end con quelli della rbegin
-		cout<<"palindroma"<<endl;
-	else
-		cout<<"non e' palindroma"<<endl;	
-	*/
+	cout<<"VERSIONE 1 TEMPO:"<<runTimeMillisec_1<<endl;
+	cout<<"VERSIONE 2 TEMPO:"<<runTimeMillisec_2<<endl;
+	cout<<"VERSIONE 3 TEMPO:"<<runTimeMillisec_3<<endl;
+
 	return 0;
 }
 
@@ -96,3 +55,24 @@ bool checkPalindrome_v2(const string &theString){
 bool checkPalindrome_v3(const string &theString){
 	return equal(theString.begin(),theString.begin()+theString.size()/2,theString.rbegin());
 }
+
+int doTest(vector<string> &toCheck,bool (*checkPalindromeFunz)(const string&)){
+	clock_t t_start,t_stop;
+	bool boolReturn=false,boolTest=true;
+	int count=0;
+	
+	t_start = clock();
+	for(int i=0;i<BENCH_ITERATIONS;i++){
+		for(int j=0;j<toCheck.size();j++){
+		   boolReturn=checkPalindromeFunz(toCheck[j]); 
+		   if(boolReturn==boolTest){
+			  count++;
+		   }
+		}
+    }
+	t_stop = clock();
+	
+	int temp=(((double)(t_stop-t_start))/CLOCKS_PER_SEC)*1000;
+	cout<<count<<endl;
+	return temp;
+} 
